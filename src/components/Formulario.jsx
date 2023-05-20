728a910c330ae4db05046dbb22b11e97e179f0f9
Bladimiro Ricaurte <bladimirolcd@gmal.com>
2023-05-20 11:40:30 -0500

    Modificar - no funciona

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,10 +1,87 @@
 import React, {useState,useEffect} from 'react'
 import {db} from "../firebase"
-import { collection,doc, addDoc, onSnapshot, deleteDoc, updateDoc } from 'firebase/firestore'
+import { collection,doc, addDoc, onSnapshot, deleteDoc, updateDoc, query } from 'firebase/firestore'
 
 const Formulario = () => {
     const [nombreLibro, setNombreLibro] = useState('')
     const [nombreAutor, setNombreAutor] = useState('')
+    const [listaLibros, setListaLibros] = useState([])
+    const [id, setId] = useState('')
+    const [modoEdicion, setModoEdicion] = useState('')
+
+    useEffect(()=>{
+        const obtenerDatos = async() =>{
+            try {
+                await onSnapshot(collection(db, 'libros'), (query)=>{
+                    setListaLibros(query.doc.map((doc)=> ({...doc.data(), id:doc.id})))
+                })
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    })
+
+    const guardarLibros = async (e) => {
+        e.preventDefault()
+        try {
+            const data = await addDoc(collection(db, 'libros'),{
+                nombreLibro:nombreLibro,
+                nombreAutor:nombreAutor
+            })
+
+            setListaLibros([...listaLibros, {
+                nombreLibro: nombreLibro,
+                nombreAutor: nombreAutor,
+                id: data.id
+            }])
+
+            setNombreLibro('')
+            setNombreAutor('')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const eliminar = async id =>{
+        try {        
+            await deleteDoc(doc(db, 'libros', id))        
+        } catch (error) {        
+         console.log(error)        
+        }        
+    }
+
+    const editar = item=> {
+        setNombreLibro(item.nombreLibro)
+        setNombreAutor(item.nombreAutor)
+        setId(item.id)
+        setModoEdicion(true)
+    }
+
+    const editarLibros = async e =>{
+        e.preventDefault();
+        try {
+            const docRef = doc(db, 'libros', id)
+            await updateDoc(docRef, {
+                nombreLibro: nombreLibro,
+                nombreAutor: nombreAutor
+            })
+
+            const nuevoArray = listaLibros.map(
+                item=> item.id === id ? {id:id, nombreLibro:nombreLibro, 
+                    nombreAutor:nombreAutor}:item
+            )
+
+            setListaLibros(nuevoArray)
+            setNombreAutor('')
+            setNombreLibro('')
+            setId('')
+            setModoEdicion(false)
+
+        } catch (error) {
+            
+        }
+    }
+
   return (
     <div className='container mt-5'>
         <h1 className='text-center'>Crud de libros</h1>
@@ -13,19 +90,26 @@ const Formulario = () => {
             <div className="col-8">
                 <h4 className='text-center'>Listado de libros</h4>
                 <ul className='list-group'>
-                    <li className='list-goup-item'>
-                        <span className='lead'>Test</span>
-                        <button className='btn btn-danger btn-sm float-end mx-2'>Eliminar</button>
-                        <button className='btn btn-warning btn-sm float-end mx-2'>Editar</button>
-                    </li>
+                    {
+                        listaLibros.map(item=>(
+                            <li className='list-goup-item' key={item.id}>
+                                <span className='lead'>{item.nombreLibro} - {item.nombreAutor}</span>
+                                <button className='btn btn-danger btn-sm float-end mx-2'onClick={()=>eliminar(item.id)}>Eliminar</button>
+                                <button className='btn btn-warning btn-sm float-end mx-2' onClick={()=>editar(item)}>Editar</button>
+                            </li>
+                        ))
+                    }
+                    
                 </ul>
         
             </div>
             <div className="col-4">
                 <h4 className='text-center'>Agregar libros</h4>
-                <form action="">
-                    <input type="text" className='form-control mb-2' placeholder='Ingrese el nombre del libro'/>
-                    <input type="text" className='form-control mb-2' placeholder='Ingrese el autor del libro'/>
+                <form onSubmit={guardarLibros}>
+                    <input type="text" className='form-control mb-2' placeholder='Ingrese el nombre del libro' 
+                    value={nombreLibro} onChange={(e)=>setNombreLibro(e.target.value)}/>
+                    <input type="text" className='form-control mb-2' placeholder='Ingrese el autor del libro'
+                    value={nombreAutor} onChange={(e)=>setNombreAutor(e.target.value)}/>
                     <button className='btn btn-primary btn-block'>Agregar</button>
                 </form>
             </div>
